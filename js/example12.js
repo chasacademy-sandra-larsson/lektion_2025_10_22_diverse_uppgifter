5450a344af85dba9ce0a481de00b2b73ac4b0c57
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-10-23 15:12:48 +0200

    instructions example12

--- js/example12.js
+++ js/example12.js
@@ -1,5 +1,24 @@
-// Hur kan jag veta vilket element jag klickar på?
-
-// Lägg till alla färgerna i listan "myList"
     
+    const ul = document.getElementById("myList");
+   
+    
+    const colors = [
+        "Red", "Blue", "Green", "Yellow", "Orange", 
+        "Purple", "Pink", "Brown", "Black", "White",
+        "Gray", "Cyan", "Magenta", "Lime", "Indigo",
+        "Violet", "Turquoise", "Gold", "Silver", "Maroon"
+      ];
+    
+    
+   // Lägg till alla färgerna i listan "myList"
+
+   
+
+    // Alternativ 1 - Lägg en eventlyssnare på alla li-element
+    
+ 
+    
+    // Alternativ 2 - Använd eventdelegation
+    // Man lägger endast en eventlyssnare på parent och sen "delegerar ner vilket event som skedde"
+
    
